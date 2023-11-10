21953241b41d8f907ed6bb2aeb7639a382c3ed62
Некит Ащеулов <xeek1337@gmail.com>
2023-11-10 14:47:10 +0300

    Добавьте файлы проекта.

---
+++ ToyShop/Pages/AuthorizationPages.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToyShop.ClassHelper;
+
+namespace ToyShop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPages.xaml
+    /// </summary>
+    public partial class AuthorizationPages : Page
+    {
+        public AuthorizationPages()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            FrameData.frame.Navigate(new RegistrationPages());
+        }
+    }
+}
